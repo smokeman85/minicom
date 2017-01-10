0c16d87906667f2f1ac85d4b9c77355e50b2bad6
Dmitry Pichugin <smokeman85@gmail.com>
2017-01-10 22:04:38 +0300

    scheme_inter.c: Get absolute path of init.scm

--- src/scheme_inter.c
+++ src/scheme_inter.c
@@ -1,9 +1,53 @@
-
+#include <unistd.h>
+#include <limits.h>
 #include "minicom.h"
 #include "intl.h"
 #include "scheme_inter.h"
 #include "scheme.h"
 #include "scheme-private.h"
+
+extern char *__progname;
+
+static void debug(char *s, ...)
+{
+	char lala[80];
+	va_list ap;
+
+	va_start(ap, s);
+	vsnprintf(lala, sizeof(lala), s, ap);
+	va_end(ap);
+	write(2, lala, strlen(lala));
+}
+
+static int get_path_scheme(char *app_path, char *init_file)
+{
+	ssize_t count;
+
+	memset(app_path, 0, PATH_MAX);
+	count = readlink("/proc/self/exe", app_path, PATH_MAX);
+	if(count == -1)
+		return -1;
+
+	app_path[count - strlen(__progname)] = '\0';
+	strcat(app_path, init_file);
+
+	return 0;
+}
+
+static void scheme_script_init(scheme *sc, char *init_file)
+{
+	FILE *finit;
+
+	scheme_init(sc);
+
+	scheme_set_input_port_file(sc, stdin);
+	scheme_set_output_port_file(sc, stdout);
+
+	finit = fopen(init_file, "r");
+	scheme_load_file(sc, finit);
+	fclose(finit);
+}
+
 
 void scheme_script_run()
 {
@@ -11,20 +55,16 @@ void scheme_script_run()
 	int done = 0;
 	int n;
 
-	FILE *finit;
 	scheme sc;
 	char script[256];
+	char app_path[PATH_MAX];
 
-	scheme_init(&sc);
-	
-	scheme_set_input_port_file(&sc, stdin);
-	scheme_set_output_port_file(&sc, stdout);
+	if (get_path_scheme(app_path, "init.scm") == -1)
+		return;
 
-        finit = fopen("init.scm", "r");
-	scheme_load_file(&sc, finit);
-	fclose(finit);
+	scheme_script_init(&sc, app_path);
 	
-	w = mc_wopen(10, 5, 70, 10, BDOUBLE, stdattr, mfcolor, mbcolor, 0, 0, 1);
+	w = mc_wopen(10, 15, 70, 20, BDOUBLE, stdattr, mfcolor, mbcolor, 0, 0, 1);
 	mc_wtitle(w, TMID, "Run scheme script");
 	mc_wputs(w, "\n");
 	mc_wprintf(w, "%s %s\n", "A - script:", "");
