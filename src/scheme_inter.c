83ddd5b91dbb88ff257f25abca36776e8290538c
Dmitry Pichugin <smokeman85@gmail.com>
2017-01-20 21:17:58 +0300

    src/scheme_inter.c: Change path of init.scm to /etc/...

--- src/scheme_inter.c
+++ src/scheme_inter.c
@@ -11,6 +11,9 @@ extern char *__progname;
 #define A_BUTTON_MSG "A - script:"
 #define WINDOW_MSG "Run scheme script (Return to run, ESC to stop)"
 
+#define MINICOM_INIT_SCRIPT "/etc/minicom/init.scm"
+#define MINICOM_SCRIPT_PATH "/etc/minicom/script/"
+
 static void debug(char *s, ...)
 {
 	char lala[80];
@@ -20,21 +23,6 @@ static void debug(char *s, ...)
 	vsnprintf(lala, sizeof(lala), s, ap);
 	va_end(ap);
 	write(2, lala, strlen(lala));
-}
-
-static int get_path_scheme(char *app_path, char *init_file)
-{
-	ssize_t count;
-
-	memset(app_path, 0, PATH_MAX);
-	count = readlink("/proc/self/exe", app_path, PATH_MAX);
-	if(count == -1)
-		return -1;
-
-	app_path[count - strlen(__progname)] = '\0';
-	strcat(app_path, init_file);
-
-	return 0;
 }
 
 static void scheme_script_init(scheme *sc, char *init_file)
@@ -83,12 +71,8 @@ void scheme_script_run()
 
 	scheme sc;
 	char script[256];
-	char app_path[PATH_MAX];
 
-	if (get_path_scheme(app_path, "init.scm") == -1)
-		return;
-
-	scheme_script_init(&sc, app_path);
+	scheme_script_init(&sc, MINICOM_INIT_SCRIPT);
 
 	w = scheme_init_gui();
 	while (!done) {
